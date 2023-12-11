aef9588977a10cfb24eccb34cd04c2a93313aeb4
davidvazgon26 <davidvazgon@hotmail.com>
2023-12-11 00:08:53 -0600

    ejercicios de paso de parametros o argumentos por consola en clase shop

---
+++ Challenges/JavaOracleOCA1Z0819/Shop.java
@@ -0,0 +1,23 @@
+package Challenges.JavaOracleOCA1Z0819;
+
+public class Shop {
+
+    public static void main(String[] args) {
+        System.out.println("Comenrtario dentro de main");
+        System.out.println("Valor introducido: " + args[0]);
+        System.out.println("Valor 2 introducido: " + args[1]);
+    }
+
+}
+
+/**
+ * Para correr el codigo en VSC recuerda correrlo con el Run que aparece dentro
+ * del codigo, en este caso arriba del main\
+ * 
+ * 
+ * El arreglo de string permite recibir valores para presentar , por ejemplo en
+ * la consola, cada uno es separado por espacio
+ * 
+ * ejemplo, compilas con javac y ejecutas con java, ej: $ java
+ * Challenges/JavaOracleOCA1Z0819/Shop David
+ */
