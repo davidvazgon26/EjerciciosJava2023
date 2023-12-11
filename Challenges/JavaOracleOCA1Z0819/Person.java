0116d80aa7efb5653c37ddd8c56804aed023b1b9
davidvazgon26 <davidvazgon@hotmail.com>
2023-12-10 23:43:46 -0600

    comentaris en java para documentar el codigo

--- Challenges/JavaOracleOCA1Z0819/Person.java
+++ Challenges/JavaOracleOCA1Z0819/Person.java
@@ -3,7 +3,14 @@ package Challenges.JavaOracleOCA1Z0819;
 public class Person {
     String name;
 
+    /**
+     * Comentarios para la documentacion del codigo
+     * 
+     * @return
+     */
     public String getName() {
+        // this.getName();
+
         return name;
     }
 
