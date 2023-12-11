0e915aa2c6dcf2d6c233e240a8498d621ff380f5
davidvazgon26 <davidvazgon@hotmail.com>
2023-12-10 23:57:14 -0600

    agregando comentarios necesarios a la clase de ejemplo

--- Challenges/JavaOracleOCA1Z0819/Person.java
+++ Challenges/JavaOracleOCA1Z0819/Person.java
@@ -1,12 +1,15 @@
 package Challenges.JavaOracleOCA1Z0819;
 
+/**
+ * @author David Vazquez
+ */
 public class Person {
-    String name;
+    String name = "David";
 
     /**
      * Comentarios para la documentacion del codigo
      * 
-     * @return
+     * @return devuelve un string
      */
     public String getName() {
         // this.getName();
@@ -15,3 +18,12 @@ public class Person {
     }
 
 }
+
+/**
+ * Ojo Java no exige mencionar el modificador de acceso en el archivo si es una
+ * clase unica.
+ * 
+ * Si en el archivo existe mas de una clase, una de ellas debe ser publica y a
+ * la que se le asigne el modificador public debe llevar el mismo nombre del
+ * archivo.
+ */
