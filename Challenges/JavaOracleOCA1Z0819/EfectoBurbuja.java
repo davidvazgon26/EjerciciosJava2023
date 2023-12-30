bf621b145a58f1337ea07e178c3dbe1e3755395c
davidvazgon26 <davidvazgon@hotmail.com>
2023-12-29 19:41:57 -0600

    ejercicios con for, foreach, arreglos y ordenamientos

--- Challenges/JavaOracleOCA1Z0819/EfectoBurbuja.java
+++ Challenges/JavaOracleOCA1Z0819/EfectoBurbuja.java
@@ -1,5 +1,9 @@
 package Challenges.JavaOracleOCA1Z0819;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class EfectoBurbuja {
     public static void main(String[] args) {
 
@@ -18,5 +22,54 @@ public class EfectoBurbuja {
             System.out.println(i);
             System.out.println(j);
         }
+
+        System.out.println("Usando el buble: ");
+        int[] miArray = { 15, 28, 13, 10, 9, 44, 16, 5 };
+        int[] result;
+
+        result = bubble(miArray);
+        System.out.println(result.length);
+        System.out.println(result[0]);
+        System.out.println(result[result.length - 1]);
+        System.out.println(Arrays.toString(result));
+
+        System.out.println("Mostrar el uso de forEach con arreglos");
+
+        String[] nombres = new String[3];
+        nombres[0] = "David";
+        nombres[1] = "Ale";
+        nombres[2] = "Juan";
+
+        for (String elemento : nombres) {
+            System.out.println(elemento);
+        }
+
+        System.out.println("Mostrar el uso de forEach con listas");
+        List<String> listaNombres = new ArrayList<String>();
+        listaNombres.add("Primer nombre");
+        listaNombres.add("David");
+        listaNombres.add("Tercer nombre");
+        listaNombres.add("Cuarto nombre");
+
+        for (String nombre : listaNombres) {
+            if ("David".equalsIgnoreCase(nombre)) {
+                continue;
+            }
+            System.out.println(nombre);
+        }
+    }
+
+    public static int[] bubble(int[] A) {
+        int i, j, aux;
+        for (i = 0; i < A.length - 1; i++) {
+            for (j = 0; j < A.length - i - 1; j++) {
+                if (A[j + 1] < A[j]) {
+                    aux = A[j + 1];
+                    A[j + 1] = A[j];
+                    A[j] = aux;
+                }
+            }
+        }
+        return A;
     }
 }
