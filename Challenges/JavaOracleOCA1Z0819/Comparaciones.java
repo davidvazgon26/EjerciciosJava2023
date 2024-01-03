2176f89df3fe7c6c76e618fb73468920999b4a01
davidvazgon26 <davidvazgon@hotmail.com>
2024-01-02 23:48:07 -0600

    difrencia entre primitivos y no primitivos

---
+++ Challenges/JavaOracleOCA1Z0819/Comparaciones.java
@@ -0,0 +1,24 @@
+package Challenges.JavaOracleOCA1Z0819;
+
+public class Comparaciones {
+
+    public static void main(String[] args) {
+        System.out.println("Comparando valores primitivos");
+
+        int a = 10;
+        int b = 10;
+
+        System.out.println("a y b son iguales? " + (a == b));
+
+        System.out.println("*********************************");
+        System.out.println("Comparando objetos");
+        // 2 formas distintas de crear un objeto
+        Integer z = 15;
+        // Integer y = new Integer(15); // Deprecado
+        Integer y = Integer.valueOf(15);
+
+        System.out.println("Los objetos son iguales? " + (z == y)); // Con el constructor daria falso
+        System.out.println("Comparando objetos, es: " + (z.equals(y)));
+    }
+
+}
