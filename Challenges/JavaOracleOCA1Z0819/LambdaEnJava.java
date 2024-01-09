f02b4cdd04db1647b2bc26cda5e75efe6d9b6af2
davidvazgon26 <davidvazgon@hotmail.com>
2024-01-08 23:38:39 -0600

    ejercicios de lambda en java

---
+++ Challenges/JavaOracleOCA1Z0819/LambdaEnJava.java
@@ -0,0 +1,16 @@
+package Challenges.JavaOracleOCA1Z0819;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LambdaEnJava {
+    public static void main(String[] args) {
+        List<String> people = new ArrayList<>();
+        people.add("David");
+        people.add("Lucas");
+        people.add("Martha");
+        people.add("Gabriel");
+
+        people.forEach(el -> System.out.println(el));
+    }
+}
